151aafb353c06ea1caf4f4e2aa54a83335dee0c2
S Akhileswara Reddy <akhileswarareddy999@gmail.com>
2026-01-01 18:31:25 +0530

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,7 +16,7 @@ app.use(express.json());
 connectDB();
 
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://informate.onrender.com', credentials: true }));
 
 
 
