223ec3d6e0067b8eda8a6896ddd5c53c1ecc4d90
Akhileswar936 <akhileswarareddy999@gmail.com>
2026-01-16 01:17:52 +0530

    update

--- frontend/src/pages/Navbar.tsx
+++ frontend/src/pages/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="bg-[rgba(255,255,255,0.3)] py-2 text-white capitalize text-black">
+      <nav className="bg-green-300 py-2 text-white capitalize">
         <div className="flex justify-between items-center px-4 md:px-8">
           <h2 className="text-xl font-semibold">Informate</h2>
 
