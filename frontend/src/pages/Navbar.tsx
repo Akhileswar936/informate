a62c3536d8a69305b5b0e77a25944c8fb7986208
Akhileswar936 <akhileswarareddy999@gmail.com>
2026-01-16 01:31:41 +0530

    update

--- frontend/src/pages/Navbar.tsx
+++ frontend/src/pages/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className="bg-violet-400 backdrop-blur-md py-2 text-gray-800 capitalize">
+      <nav className="bg-green-600 py-2 text-white capitalize">
         <div className="flex justify-between items-center px-4 md:px-8">
           <h2 className="text-xl font-semibold">Informate</h2>
 
@@ -19,18 +19,18 @@ const Navbar = () => {
           </button>
 
           <ul className="hidden md:flex gap-6">
-            <Link to="/home"><li className="hover:text-gray-500">Home</li></Link>
-            <Link to="/feed"><li className="hover:text-gray-500">Feed</li></Link>
-            <Link to="/bookmark"><li className="hover:text-gray-500">Bookmarks</li></Link>
-            <Link to="/connects"><li className="hover:text-gray-500">Connect</li></Link>
-            <Link to="/profile"><li className="hover:text-gray-500">Profile</li></Link>
-            <Link to="/about"><li className="hover:text-gray-500">About</li></Link>
-            <Link to="/logout"><li className="hover:text-gray-500">Logout</li></Link>
+            <Link to="/home"><li className="hover:text-gray-300">Home</li></Link>
+            <Link to="/feed"><li className="hover:text-gray-300">Feed</li></Link>
+            <Link to="/bookmark"><li className="hover:text-gray-300">Bookmarks</li></Link>
+            <Link to="/connects"><li className="hover:text-gray-300">Connect</li></Link>
+            <Link to="/profile"><li className="hover:text-gray-300">Profile</li></Link>
+            <Link to="/about"><li className="hover:text-gray-300">About</li></Link>
+            <Link to="/logout"><li className="hover:text-gray-300">Logout</li></Link>
           </ul>
         </div>
 
         {open && (
-          <ul className="flex flex-col items-center gap-4 pt-4 pb-4 md:hidden bg-white/20 backdrop-blur-md">
+          <ul className="flex flex-col items-center gap-4 pt-4 pb-4 md:hidden bg-black/40 backdrop-blur">
             <Link to="/home"><li>Home</li></Link>
             <Link to="/feed"><li>Feed</li></Link>
             <Link to="/bookmark"><li>Bookmarks</li></Link>
