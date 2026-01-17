60f8e125276449ff9bf1271d5dafd679793726f0
Akhileswar936 <akhileswarareddy999@gmail.com>
2026-01-17 18:54:12 +0530

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,7 +24,7 @@ const App = () => {
      <>
          <BrowserRouter>
                 <TokenExpirationTimer/>
-                <ToastContainer  autoClose={500}/>
+                <ToastContainer  autoClose={200}/>
                <Routes>
                     <Route path="/" element={<Intro/>}/>
                     <Route path="/register" element={<Register/>} />
